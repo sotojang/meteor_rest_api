0133c5d30851abbd57dcc448f41e0250ca8a4849
sotojang <mrjang28@gmail.com>
2017-07-17 12:58:15 +0900

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
+import { Mongo } from 'meteor/mongo'; //
 
 Meteor.startup(() => {
   // code to run on server at startup
